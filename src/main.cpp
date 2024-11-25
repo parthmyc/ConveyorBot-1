273a983a9eae39bb01a44f346233af55fa668790
parthmyc <pyaeron@gmail.com>
2024-11-25 14:20:05 -0500

    incomplete cheesy drive

--- src/main.cpp
+++ src/main.cpp
@@ -73,10 +73,109 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+
+
+
+// We apply a sinusoidal curve (twice) to the joystick input to give finer
+// control at small inputs.
+static double _turnRemapping(double iturn) {
+	double denominator = sin(PI / 2 * CD_TURN_NONLINEARITY);
+	double firstRemapIteration =
+	    sin(PI / 2 * CD_TURN_NONLINEARITY * iturn) / denominator;
+	return sin(PI / 2 * CD_TURN_NONLINEARITY * firstRemapIteration) / denominator;
+}
+
+// On each iteration of the drive controller (where we aren't point turning) we
+// constrain the accumulators to the range [-1, 1].
+double quickStopAccumlator = 0.0;
+double negInertiaAccumlator = 0.0;
+static void _updateAccumulators() {
+	if (negInertiaAccumlator > 1) {
+		negInertiaAccumlator -= 1;
+	} else if (negInertiaAccumlator < -1) {
+		negInertiaAccumlator += 1;
+	} else {
+		negInertiaAccumlator = 0;
+	}
+
+	if (quickStopAccumlator > 1) {
+		quickStopAccumlator -= 1;
+	} else if (quickStopAccumlator < -1) {
+		quickStopAccumlator += 1;
+	} else {
+		quickStopAccumlator = 0.0;
+	}
+}
+
+double prevTurn = 0.0;
+double prevThrottle = 0.0;
+void cheesyDrive(double ithrottle, double iturn) {
+	bool turnInPlace = false;
+	double linearCmd = ithrottle;
+	if (fabs(ithrottle) < DRIVE_DEADBAND && fabs(iturn) > DRIVE_DEADBAND) {
+		// The controller joysticks can output values near zero when they are
+		// not actually pressed. In the case of small inputs like this, we
+		// override the throttle value to 0.
+		linearCmd = 0.0;
+		turnInPlace = true;
+	} else if (ithrottle - prevThrottle > DRIVE_SLEW) {
+		linearCmd = prevThrottle + DRIVE_SLEW;
+	} else if (ithrottle - prevThrottle < -(DRIVE_SLEW * 2)) {
+		// We double the drive slew rate for the reverse direction to get
+		// faster stopping.
+		linearCmd = prevThrottle - (DRIVE_SLEW * 2);
+	}
+
+	double remappedTurn = _turnRemapping(iturn);
+
+	double left, right;
+	if (turnInPlace) {
+		// The remappedTurn value is squared when turning in place. This
+		// provides even more fine control over small speed values.
+		left = remappedTurn * std::abs(remappedTurn);
+		right = -remappedTurn * std::abs(remappedTurn);
+
+		// The FRC Cheesy Drive Implementation calculated the
+		// quickStopAccumulator here:
+		// if (Math.abs(linearPower) < 0.2) {
+		// 	double alpha = 0.1;
+		// 	quickStopAccumulator = (1 - alpha) * quickStopAccumulator
+		// 			+ alpha * Util.limit(wheel, 1.0) * 5;
+		// }
+		// But I apparently left that out of my implementation? Seemed to work
+		// without it though.
+	} else {
+		double negInertiaPower = (iturn - prevTurn) * CD_NEG_INERTIA_SCALAR;
+		negInertiaAccumlator += negInertiaPower;
+
+		double angularCmd =
+		    abs(linearCmd) *  // the more linear vel, the faster we turn
+		        (remappedTurn + negInertiaAccumlator) *
+		        CD_SENSITIVITY -  // we can scale down the turning amount by a
+		                          // constant
+		    quickStopAccumlator;
+
+		right = left = linearCmd;
+		left += angularCmd;
+		right -= angularCmd;
+
+		_updateAccumulators();
+	}
+
+	controller->tank(left, right);
+
+	prevTurn = iturn;
+	prevThrottle = ithrottle;
+}
+
+
+#define left_wheels_port 1
+#define right_wheels_port 2
+
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::Motor left_wheels(left_wheels_port);    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::Motor right_wheels(right_wheels_port, true);  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 
 	while (true) {
@@ -84,6 +183,17 @@ void opcontrol() {
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
+		// Cheesy Drive
+		double iturn = master.get_analog(ANALOG_RIGHT_X);
+		double ithrottle = master.get_analog(ANALOG_LEFT_Y);
+
+		_updateAccumulators();
+
+		cheesyDrive(ithrottle, iturn);
+		
+
+		
+		
 		// Arcade control scheme
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
