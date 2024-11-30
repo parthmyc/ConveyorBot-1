e10ce141d37cc07ed2d1608d9eff1fcd1ca5f586
parthmyc <pyaeron@gmail.com>
2024-11-30 02:07:56 -0500

    LemLib curvature drive setup

--- src/main.cpp
+++ src/main.cpp
@@ -24,16 +24,76 @@ pros::MotorGroup right_motor_group({right_motor});
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&left_motor_group,
                               &right_motor_group,
-                              10, // track width
+                              11.125, // track width
                               lemlib::Omniwheel::OLD_4, // wheel type
                               200, // rpm
                               2 // horizontal drift
 );
 
 // create sensors
+// imu
 pros::Imu imu(11);
-pros::adi::Encoder vertical_encoder('C', 'D');
+// horizontal tracking wheel encoder
 pros::adi::Encoder horizontal_encoder('A', 'B');
+// vertical tracking wheel encoder
+pros::adi::Encoder vertical_encoder('C', 'D');
+// horizontal tracking wheel
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_4, -10.75);
+// vertical tracking wheel
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::OLD_4, -5.625);
+
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizontal_tracking_wheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steer_curve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.019 // expo curve gain
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors, // odometry sensors
+						&throttle_curve,
+						&steer_curve
+
+);
 
 /**
  * A callback function for LLEMU's center button.
@@ -58,17 +118,19 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
-	while (true) { // infinite loop
-        // print measurements from the adi encoder
-        pros::lcd::print(0, "Rotation Sensor: %i", vertical_encoder.get_value());
-        // print measurements from the rotation sensor
-        pros::lcd::print(1, "Rotation Sensor: %i", horizontal_encoder.get_value());
-        pros::delay(10); // delay to save resources. DO NOT REMOVE
-    }
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 /**
@@ -117,7 +179,7 @@ void autonomous() {}
  */
 
 
-
+/**
 // We apply a sinusoidal curve (twice) to the joystick input to give finer
 // control at small inputs.
 static double _turnRemapping(double iturn) {
@@ -209,8 +271,24 @@ std::pair<double,double> cheesyDrive(double ithrottle, double iturn) {
 
 	return std::make_pair(left,right);
 }
+ */
+
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 void opcontrol() {
+    // loop forever
+    while (true) {
+        // get left y and right x positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        // move the robot
+        chassis.curvature(leftY, rightX);
+
+        // delay to save resources
+        pros::delay(25);
+    }
+	
 	/**
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 
